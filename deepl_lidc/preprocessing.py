9bd717ff046ba8f75b121d445a6db1508e792a3f
Munteanu Vadim <vadim.munteanu28@gmail.com>
2023-12-06 17:35:06 +0000

    added new label column

--- deepl_lidc/preprocessing.py
+++ deepl_lidc/preprocessing.py
@@ -1,9 +1,13 @@
 """ 
-A module with functions for preprocessing the data.
+A module with functions for preprocessing the target data.
 """
 
 import os
+import numpy as np
 import pandas as pd
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 def available_patients(path_scans):
     """
@@ -28,15 +32,18 @@ def prepare_label(path_raw_label, ids):
     #keep only the patients that are in the ids list
     df = df[df['patient_id'].isin(ids)]
     
-    #add a new column to the dataframe named 'target'.
-    #for each patient sum the malignancy scores and
-    # divide by the number of nodules.
-    # if the score is >= 3 then the patient is malignant
-    df['target'] = df.groupby('patient_id')['malignancy'].transform('sum') / df.groupby('patient_id')['malignancy'].transform('count')
-    df['target'] = df['target'].apply(lambda x: 1 if x >= 3 else 0)
-    df = df.drop(columns=['malignancy'])
-    #drop anotation_id and nodule column
-    df = df.drop(columns=['annotation_id', 'nodule'])
+    # Calculate the mean malignancy score for each nodule
+    df['mean_malignancy'] = df.groupby(['patient_id', 'nodule'])['malignancy'].transform('mean')
+
+    # Count the number of nodules for each patient
+    df['num_nodules'] = df.groupby('patient_id')['nodule'].transform('nunique')
+
+    # Apply the formula to compute 'target' for each row
+    df['target'] = sigmoid(df['num_nodules'] * (df['mean_malignancy'] / 2 - 1))
+
+    #drop the columns that are not needed anymore
+    df = df.drop(columns=['nodule', 'annotation_id', 'malignancy', 'mean_malignancy', 'num_nodules'])
+
     #drop patiants duplicates
     df = df.drop_duplicates(subset=['patient_id'])
     
