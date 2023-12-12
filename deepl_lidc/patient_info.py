07e211cdb7d840716bc3ed56bc12216e074f458c
Munteanu Vadim <vadim.munteanu28@gmail.com>
2023-12-12 18:38:40 +0000

    added plot slice

---
+++ deepl_lidc/patient_info.py
@@ -0,0 +1,38 @@
+""" 
+Module with functions that give information
+about specific patient. print statements or
+plots.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pylidc as pl
+
+def nodule_info(pid):
+    """Print information about nodules in patient.
+
+    Args:
+        pid (string): Patient id of the form "LIDC-IDRI-XXXX".
+    """
+    
+    scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).first()
+    
+    nods = scan.cluster_annotations()
+    #print number of nodules
+    print("%s has %d nodules." % (scan, len(nods)))
+
+    #print number of annotations for each nodule
+    for i,nod in enumerate(nods):
+        print("Nodule %d has %d annotations." % (i+1, len(nods[i])))
+
+def plot_slice(vol, slice_num):
+    """
+    Plot a slice of the volume
+    
+    Args:
+        vol (numpy array): Volume of the patient.
+        slice_num (int): Slice number to plot.
+    """    
+    plt.figure(figsize=(5, 5))
+    plt.imshow(vol[:,:,slice_num])
+    plt.show()
