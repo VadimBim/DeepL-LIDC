568faa8d2cc130e7cc6215d0c4e0c575a5055865
Munteanu Vadim <vadim.munteanu28@gmail.com>
2023-12-12 18:39:27 +0000

    created image_prep script

--- scripts/image_prep.py
+++ scripts/image_prep.py
@@ -2,21 +2,8 @@
 A script to extract the middle
 slice of a 3D image and save it
 """
-
-import os
-import pylidc as pl
+from deepl_lidc import patient_info
 
 pid = 'LIDC-IDRI-0012'
-scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).first()
 
-nods = scan.cluster_annotations()
-
-print("%s has %d nodules." % (scan, len(nods)))
-
-for i,nod in enumerate(nods):
-    print("Nodule %d has %d annotations." % (i+1, len(nods[i])))
-    
-vol = scan.to_volume()
-print(vol.shape)
-
-scan.visualize(annotation_groups=nods)
+patient_info.nodule_info(pid)
