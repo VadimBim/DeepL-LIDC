4bb089d6265447b37d4e1d1d6ad324760a620caa
Munteanu Vadim <vadim.munteanu28@gmail.com>
2023-12-12 19:18:02 +0000

    added show_nodule plotting

--- deepl_lidc/patient_info.py
+++ deepl_lidc/patient_info.py
@@ -36,3 +36,36 @@ def plot_slice(vol, slice_num):
     plt.figure(figsize=(5, 5))
     plt.imshow(vol[:,:,slice_num])
     plt.show()
+    
+def show_nodule(vol, bbox, mask, z_slice):
+    """Show the nodule in the volume
+
+    Args:
+        vol (np.array): 3D volume (512 x 512 x n_slices)
+        bbox (_type_) : slice of the volume used to detect where the nodule is
+        mask (_type_): mask of the nodule
+        z_slice (int, optional): vertical slice of the volume to show.
+    """
+    
+    vmin = vol.min()
+    vmax = vol.max()
+    
+    minivol = vol[bbox]
+    #keep only the slice where the nodule is
+    nodule_vol = np.where(mask == 1, minivol, vmin)
+
+    fig,ax = plt.subplots(1,3,figsize=(5,3))
+
+    ax[0].imshow(minivol[:,:,z_slice], cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
+
+    ax[1].imshow(nodule_vol[:,:,z_slice], cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
+
+    ax[2].imshow(mask[:,:,z_slice], cmap=plt.cm.gray)
+
+    #hide the axis
+    for a in ax:
+        a.axis('off')
+
+    plt.tight_layout()
+    #set title
+    plt.show()
