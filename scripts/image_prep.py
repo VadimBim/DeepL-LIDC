923ecfe992f20bdc319cd0804597e7a52354320b
Munteanu Vadim <vadim.munteanu28@gmail.com>
2023-12-14 17:22:01 +0000

    script to extract available nodules

--- scripts/image_prep.py
+++ scripts/image_prep.py
@@ -1,9 +1,22 @@
 """ 
-A script to extract the middle
-slice of a 3D image and save it
+Script to extract all the nodules from dataset
+and save them as .npy files in the data folder
 """
-from deepl_lidc import patient_info
+import os
+import numpy as np
+from deepl_lidc.preprocessing import extract_nodules, available_patients
 
-pid = 'LIDC-IDRI-0012'
+path_scans = '/home/vadim/Development/Projects/DeepL-LIDC/data/LIDC-IDRI'
+path_nodules = '/home/vadim/Development/Projects/DeepL-LIDC/data/nodules'
 
-patient_info.nodule_info(pid)
+def main():
+    patient_ids = available_patients(path_scans)
+    #for all available patient_ids extract the nodules and save them as .npy files
+    #with the name 'LIDC-IDRI-XXXX-nodule_id.npy'
+    for patient_id in patient_ids:
+        nodules = extract_nodules(patient_id)
+        for nodule_id, nodule in nodules.items():
+            np.save(os.path.join(path_nodules, nodule_id), nodule)
+
+if __name__ == '__main__':
+    main()
