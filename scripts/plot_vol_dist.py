f70fe9650a1a69d99782fd165c1457d0cb7075d4
Munteanu Vadim <vadim.munteanu28@gmail.com>
2023-12-18 18:37:54 +0000

    Update volume distribution plot

--- scripts/plot_vol_dist.py
+++ scripts/plot_vol_dist.py
@@ -11,23 +11,30 @@ df = pd.read_csv('/home/vadim/Development/Projects/DeepL-LIDC/data/merged.csv')
 #make mean volume array. group by patient, then by nodule. find mean volume for each nodule
 mean_volume = df.groupby(['patient_id', 'nodule'])[' volume'].mean()
 n_nodules = len(mean_volume)
+#number of nodules with volume < 5000
+n_5000 = len(mean_volume[mean_volume < 5000])
+#number of nodules with volume < 2000
+n_2000 = len(mean_volume[mean_volume < 2000])
 #add text in the upper right corner with the number of nodules
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 6))
 
 # Plot the full distribution
 ax1.hist(mean_volume, density=False, bins=100)
 ax1.set_xlabel('Volume (mm^3)')
-ax1.set_title('Full Distribution of Nodule Volumes')
-ax1.text(0.7, 0.8, 'n = ' + str(n_nodules), fontsize=12, transform=plt.gcf().transFigure)
+ax1.text(0.95, 0.95, 'n = ' + str(n_nodules), fontsize=12, transform=ax1.transAxes, ha='right', va='top')
 
 # Plot the distribution in range [0, 5000]
 ax2.hist(mean_volume, density=False, bins=100, range=(0, 5000))
 ax2.set_xlabel('Volume (mm^3)')
-ax2.set_title('Distribution of Nodule Volumes (0-5000)')
+ax2.text(0.95, 0.95, 'n = ' + str(n_5000), fontsize=12, transform=ax2.transAxes, ha='right', va='top')
 
-# # Hide y-axis values for both plots
-# ax1.axes.get_yaxis().set_visible(False)
-# ax2.axes.get_yaxis().set_visible(False)
+# Plot the distribution in range [0, 2000]
+ax3.hist(mean_volume, density=False, bins=100, range=(0, 2000))
+ax3.set_xlabel('Volume (mm^3)')
+ax3.text(0.95, 0.95, 'n = ' + str(n_2000), fontsize=12, transform=ax3.transAxes, ha='right', va='top')
+
+# Set the title
+fig.suptitle('Distribution of nodule volumes')
 
 # Save the figure
 plt.savefig('/home/vadim/Development/Projects/DeepL-LIDC/results/volume_distribution.png', dpi=200)
