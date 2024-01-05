66ff93d07ef5d071accc543bf70adc3cd633118c
Munteanu Vadim <vadim.munteanu28@gmail.com>
2024-01-05 17:30:46 +0000

    Add .pth files to .gitignore and implement CNN model

--- deepl_lidc/models.py
+++ deepl_lidc/models.py
@@ -24,3 +24,36 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         pred_prob = softmax(logits)
         return pred_prob
+        
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=7, stride=1, padding=0),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(10, 20, kernel_size=3, stride=1, padding=0),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(20, 40, kernel_size=3, stride=1, padding=0),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(2560, 1280),
+            nn.ReLU(),
+            nn.Linear(1280, 1280),
+            nn.ReLU(),
+            nn.Linear(1280, 1280),
+            nn.ReLU(),
+            nn.Linear(1280, 160),
+            nn.ReLU(),
+            nn.Linear(160, 5)
+        )
+
+    def forward(self, x):
+        x = self.features(x.unsqueeze(1)) #unsqueeze add channel dimension
+        x = self.classifier(x)
+        pred = softmax(x)
+        return pred
